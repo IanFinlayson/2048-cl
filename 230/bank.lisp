e65893eb13c9063bac73532977755324b95d32dd
Ian Finlayson <finlaysoni@gmail.com>
2014-04-22 21:00:57 -0400

    Working on bank sim.

--- 230/bank.lisp
+++ 230/bank.lisp
@@ -1,4 +1,11 @@
+; read data from a file supplied by the user
+(defun read-data ()
+  (format t "Enter input file:~%")
+  (with-open-file (f (read-line) :direction :input :if-does-not-exist nil)
+    (if (null f)
+      (format t "Error, file not found!~%")
+      (loop for line = (read-line f nil nil) while line collect `(,line)))))
 
 
-
+(format t "~a~%" (read-data))
 
