f96688db3f9d32a94d0d0bfadac1e11e3e3c361b
Ian Finlayson <finlaysoni@gmail.com>
2014-04-23 21:21:18 -0400

    ugh

--- 230/bank.lisp
+++ 230/bank.lisp
@@ -27,18 +27,28 @@
     (* (+ (floor (/ now 100)) 1) 100)
     (+ 1 now)))
 
+; queue functions
+(defun enqueue (q item)
+  (append q (list `(,(car item) ,(+ (cadr item) (caddr item))))))
+(defun front (q)
+  (car q))
+(defun dequeue (q)
+  (cdr q))
+
 ; main program simulation
-(defun sim (minute queue data)
-  (format t "Time is ~a~%" minute)
+(defun sim (minute queue data teller)
+  ;(format t "Time is ~a, queue is ~a~%" minute queue)
   (cond ((and (not (null data)) (= minute (cadar data)))
         (progn
-          (format t "~a got in line at ~a.~%" (caar data) (cadar data))
-          (sim minute (cons (car data) queue) (cdr data))))
+          (format t "~a got in line at ~a.~%" (caar data) minute)
+          (sim minute (enqueue queue (car data)) (cdr data) teller)))
+        ((and (not (null teller)) (= minute (cadr teller)))
+         (progn
+           (format t "~a is done at ~a.~%" (caar queue) minute)
+           (sim minute (dequeue queue) data (front queue))))
         ((>= minute 1700) nil)
-        (t (sim (inc-time minute) queue data))))
-  
-  
+        (t (sim (inc-time minute) queue data teller))))
 
 ; start it all in motion
-(sim 900 '() (read-data))
+(sim 900 '() (read-data) nil)
 
