a9568bf8b41febb2a0576d16ab727b22ea62cac6
Ian Finlayson <finlaysoni@gmail.com>
2014-04-28 21:20:57 -0400

    working on scheduler.

--- 230/schedule.lisp
+++ 230/schedule.lisp
@@ -16,16 +16,35 @@
         (loop for line = (read-line f nil nil) while line
           collect (parse line)))))
 
-; make a graph out of the list of courses
+; make a list of just the nodes
+(defun make-nodes (data)
+  (loop for course in data
+    collect (first course)))
 
+; make an adjacency matrix
+(defun make-matrix (data)
+  (loop for a in data collect
+    (loop for b in data collect
+      (if (member (first b) (rest a) :test #'string=) t nil))))
 
+; returns a list of courses with no edges coming in
+(defun find-active (matrix)
+  (loop for i from 0 to (length matrix) collect
+    (if (member t (nth i matrix)) i)))
+
+; do the topological sorting
+(defun top-sort (nodes matrix)
+  (let ((ordering '())
+        (active (find-active matrix)))
+    ))
+    
 
 ; run it
 (defun main (fname)
-  (format t "~a~%" (read-data fname)))
-
-  
-  
+  (let* ((data (read-data fname))
+         (nodes (make-nodes data))
+         (matrix (make-nodes data)))
+    (top-sort nodes matrix)))
 
 ; get the argument
 ;(if (< (length *posix-argv*) 2)
