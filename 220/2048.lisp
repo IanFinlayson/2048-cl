66bad9bb3c09065641bcb926a976402935ce1ad3
Ian Finlayson <finlaysoni@gmail.com>
2014-04-29 13:08:32 -0400

    Added random values and full test.

--- 220/2048.lisp
+++ 220/2048.lisp
@@ -47,13 +47,24 @@
   (format t "down~%")
   t)
 
+; this method checks if the board is full
+(defmethod fullp ((this board-widget))
+  (let ((full t))
+    (loop for row from 0 to 3 do
+      (loop for col from 0 to 3 do
+        (when (= (index (board this) row col) 0)
+          (setf full nil))))
+    full))
+
 ; this method adds a random 2 or 4 cell into an empty space
 (defmethod add-random ((this board-widget))
-  (let ((row (random 4)) (col (random 4)))
-    (if (= (index (board this) row col) 0)
-      (setf (nth col (nth row (board this))) (if (= (random 2) 0) 2 4))
-      (add-random this))))
+  (when (not (fullp this))
+    (let ((row (random 4)) (col (random 4)))
+      (if (= (index (board this) row col) 0)
+        (setf (nth col (nth row (board this))) (if (= (random 2) 0) 2 4))
+        (add-random this)))))
 
+; this is the main key press handler for the game
 (defmethod key-press-event ((this board-widget) event)
   (when (cond ((= (#_key event) 16777234) (left this))
         ((= (#_key event) 16777235) (up this))
@@ -72,7 +83,11 @@
       (loop for col from 0 to 3 collect 0)))
   (setf (board-labels this)
     (loop for row from 0 to 3 collect
-      (loop for col from 0 to 3 collect (#_new QLabel "Arf" this))))
+      (loop for col from 0 to 3 collect (#_new QLabel "0" this))))
+  ; add two randoms and update the board
+  (add-random this)
+  (add-random this)
+  (update-board this)
   ; initialize the space (x, y, w, h) and title
   (#_setGeometry this 200 200 500 500)
   (#_setWindowTitle this "2048 Game")
