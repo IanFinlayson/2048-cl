015b112a803580c0785582e73d5efc3a96567f8b
Ian Finlayson <finlaysoni@gmail.com>
2014-04-30 11:44:34 -0400

    Left is almost working...

--- 220/2048.lisp
+++ 220/2048.lisp
@@ -14,9 +14,43 @@
 ; allow us to read qt symbols directly
 (named-readtables:in-readtable :qt)
 
+; constants
+(defconstant +center-text+ 132)
+(defconstant +font-size+ 28)
+(defconstant +window-size+ 450)
+(defconstant +cell-size+ 100)
+(defconstant +left-key+ 16777234)
+(defconstant +up-key+ 16777235)
+(defconstant +right-key+ 16777236)
+(defconstant +down-key+ 16777237)
+
 ; indexes a 2d list
 (defun index (l row col)
   (nth col (nth row l)))
+
+; this function returns the background color for a given value
+(defun bg-color (val)
+  (case val
+    (0 (#_new QColor 204 192 179))
+    (2 (#_new QColor 238 228 218))
+    (4 (#_new QColor 237 224 200))
+    (8 (#_new QColor 242 177 121))
+    (16 (#_new QColor 245 149 99))
+    (32 (#_new QColor 246 124 95))
+    (64 (#_new QColor 246 94 59))
+    (128 (#_new QColor 237 207 114))
+    (256 (#_new QColor 237 204 97))
+    (512 (#_new QColor 237 200 80))
+    (1024 (#_new QColor 237 197 63))
+    (2048 (#_new QColor 237 194 46))
+    (otherwise (#_new QColor 47 43 37))))
+
+; this function returns the text color for a given value
+(defun text-color (val)
+  (case val
+    (0 (#_new QColor 204 192 179))
+    ((2 4) (#_new QColor 119 110 101))
+    (otherwise (#_new QColor 249 246 242))))
 
 ; this class represents a single cell in the game
 (defclass cell-widget ()
@@ -29,14 +63,13 @@
 (defmethod paint-event ((this cell-widget) paint-event)
   (declare (ignore paint-event))
   (let ((painter (#_new QPainter this)))
-    (format t "Paint!~%")
-      (#_setPen painter (#_new QColor 242 177 121))
-      (#_setBrush painter (#_new QBrush (#_new QColor 242 177 121)))
-      (#_drawRect painter (#_new QRect 0 0 100 100))
-      (#_setPen painter (#_new QColor 255 255 255))
-      (#_setFont painter (#_new QFont "Arial" 28))
-      (#_drawText painter (#_new QRect 0 0 100 100) 132 (write-to-string (value this)))
-      (#_end painter)))
+    (#_setPen painter (bg-color (value this)))
+    (#_setBrush painter (#_new QBrush (bg-color (value this))))
+    (#_drawRect painter (#_new QRect 0 0 +cell-size+ +cell-size+))
+    (#_setPen painter (text-color (value this)))
+    (#_setFont painter (#_new QFont "Arial" +font-size+))
+    (#_drawText painter (#_new QRect 0 0 +cell-size+ +cell-size+) +center-text+ (write-to-string (value this)))
+    (#_end painter)))
 
 ; constructor for cell
 (defmethod initialize-instance :after ((this cell-widget) &key val)
@@ -45,8 +78,7 @@
 
 ; make a board-widget class that derives from QWidget
 (defclass board-widget ()
-  ((board :accessor board)
-   (board-labels :accessor board-labels))
+  ((board :accessor board))
   (:metaclass qt-class)
   (:qt-superclass "QWidget")
   (:override ("keyPressEvent" key-press-event)))
@@ -55,14 +87,32 @@
 (defmethod update-board ((this board-widget))
   (loop for row from 0 to 3 do
     (loop for col from 0 to 3 do
-      (format t "TODO~%"))))
-          ;      (#_setText (index (board-labels this) row col) (write-to-string (index (board this) row col))))))
+      (#_update (index (board this) row col)))))
 
 ; these methods each move the board in a specific direction
 ; they return whether or not they made any change at all
 (defmethod left ((this board-widget))
-  (format t "left~%")
-  t)
+  (let ((change nil))
+    ; for each row
+    (loop for row from 0 to 3 do
+      ; for each column in reverse
+      (loop for col from 1 to 3 do
+        ; find v (current) and l (left)
+        (let ((v (value (index (board this) row col)))
+              (l (value (index (board this) row (- col 1)))))
+          ; when one to the left is empty, move it
+          (when (and (= l 0) (/= v 0))
+            (setf (value (index (board this) row (- col 1))) v)
+            (setf (value (index (board this) row col)) 0)
+            (setf change t))
+          ; when one to the left is equal, combine it
+          (when (and (= l v) (/= v 0))
+            (setf (value (index (board this) row (- col 1))) (* v 2))
+            (setf (value (index (board this) row col)) 0)
+            (setf change t)))))
+    change))
+
+
 (defmethod right ((this board-widget))
   (format t "right~%")
   t)
@@ -78,7 +128,7 @@
   (let ((full t))
     (loop for row from 0 to 3 do
       (loop for col from 0 to 3 do
-        (when (= (index (board this) row col) 0)
+        (when (= (value (index (board this) row col)) 0)
           (setf full nil))))
     full))
 
@@ -86,16 +136,16 @@
 (defmethod add-random ((this board-widget))
   (when (not (fullp this))
     (let ((row (random 4)) (col (random 4)))
-      (if (= (index (board this) row col) 0)
-        (setf (nth col (nth row (board this))) (if (= (random 2) 0) 2 4))
+      (if (= (value (index (board this) row col)) 0)
+        (setf (value (nth col (nth row (board this)))) (if (= (random 2) 0) 2 4))
         (add-random this)))))
 
 ; this is the main key press handler for the game
 (defmethod key-press-event ((this board-widget) event)
-  (when (cond ((= (#_key event) 16777234) (left this))
-        ((= (#_key event) 16777235) (up this))
-        ((= (#_key event) 16777236) (right this))
-        ((= (#_key event) 16777237) (down this))
+  (when (cond ((= (#_key event) +left-key+) (left this))
+        ((= (#_key event) +up-key+) (up this))
+        ((= (#_key event) +right-key+) (right this))
+        ((= (#_key event) +down-key+) (down this))
         (t nil))
     (add-random this))
   (update-board this))
@@ -106,30 +156,26 @@
   ; clear the board
   (setf (board this)
     (loop for row from 0 to 3 collect
-      (loop for col from 0 to 3 collect 0)))
-  (setf (board-labels this)
-    (loop for row from 0 to 3 collect
-      ;(loop for col from 0 to 3 collect (#_new QLabel "0" this))))
-      (loop for col from 0 to 3 collect (make-instance 'cell-widget :val 0))))
+      (loop for col from 0 to 3 collect
+        (make-instance 'cell-widget :val 0))))
   ; add two randoms and update the board
   (add-random this)
   (add-random this)
   (update-board this)
   ; initialize the space (x, y, w, h) and title
-  (#_setGeometry this 200 200 500 500)
   (#_setWindowTitle this "2048 Game")
   ; add a grid layout of labels
   (let ((grid (#_new QGridLayout this)))
     (loop for row from 0 to 3 do
       (loop for col from 0 to 3 do
-        (#_addWidget grid (index (board-labels this) row col) row col)))
+        (#_addWidget grid (index (board this) row col) row col)))
     (#_setLayout this grid)))
 
 ; make a new window
 (defun main ()
   (with-main-window (window (make-instance 'board-widget))
     (#_setStyleSheet window "background-color: #C2B39A;")
-    (#_setFixedSize window 450 450)))
+    (#_setFixedSize window +window-size+ +window-size+)))
 
 ; call the main function
 (setf *random-state* (make-random-state t))
